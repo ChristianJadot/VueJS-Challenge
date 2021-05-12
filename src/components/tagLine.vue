2ae9eada1286d101cf3fdb8807a1625d18345e10
ChristianJadot <christian.jadot@gmail.com>
2021-05-12 16:57:17 +0200

    src comp tagLine vue

--- src/components/tagLine.vue
+++ src/components/tagLine.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">Merci beaucoup à Marcelo pour son aide</a>.
-    </p>
+    <p class="lead font-weight-bold">Un nouveau lieu de rencontre, un nouveau lieu où bien manger en toute décontraction...</p>
     <h3>Essential Links</h3>
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
