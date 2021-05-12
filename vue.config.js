a2c0bac35ac2ad6f8de1c59d78058fcd1bb39d76
ChristianJadot <christian.jadot@gmail.com>
2021-05-12 17:22:50 +0200

    vue config js

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// Put this into /vue.config.js
+module.exports = {
+    chainWebpack: config => {
+      config
+        .plugin('html')
+        .tap(args => {
+        args[0].title = 'BeCool Cooking';	// Replace your title here
+        return args;
+      });
+    }
+  };
