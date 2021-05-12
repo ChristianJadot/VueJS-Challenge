8464ff0bdde8755eaf868f983d27617f8fdb82c7
ChristianJadot <christian.jadot@gmail.com>
2021-05-12 17:23:05 +0200

    src comp tagLine vue

--- src/components/tagLine.vue
+++ src/components/tagLine.vue
@@ -2,14 +2,6 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p class="lead font-weight-bold">Un nouveau lieu de rencontre, un nouveau lieu où bien manger en toute décontraction...</p>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
   </div>
 </template>
 
